648f99ffb1fee5996121d82096e75071dcca6bde
annisarachma <saa.annisa15@gmail.com>
2024-10-13 22:37:46 +0700

    add description

--- src/components/inner_bg/innerBg.tsx
+++ src/components/inner_bg/innerBg.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function InnerBg() {
   return (
     <>
-      <div className="bg-[#E4E4E4] h-96 w-[1240px] sm:w-[400px]"></div>
+      <div className="text-gray-100 p-16">
+        <div>
+          <h1>Hi, I'm</h1>
+          <h1>Annisa</h1>
+        </div>
+        <p className="w-full max-w-[800px] min-w-[300px] text-center text-xl" >A <span className="font-bold">web developer</span> and <span className="font-bold">designer</span> who love using code to merge design and technology, creating aesthetically pleasing and functional outcomes.</p>
+      </div>
     </>
   );
 }
