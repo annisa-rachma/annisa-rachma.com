8990ca8df0cf62c528462895e1f3c5ad866442b0
annisarachma <saa.annisa15@gmail.com>
2024-10-19 16:28:01 +0700

    styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,8 @@ import { FaRegEnvelope, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    
-    <div className="layout text-stone-400 font-lato text-[1rem]  flex justify-between h-14 items-end   lg:px-44 md:px-10 bg-[#171717] bg-noise">
+
+    <div className="layout text-stone-400 font-lato text-[1rem] left-1/2 -translate-x-1/2 fixed flex justify-between h-14 items-end w-10/12 bg-[#171717] bg-noise">
       <div>
         <ul className="flex gap-8">
           <a href="" className="hover:text-white text-[1.25rem]">
--- src/components/PageHome/PageHome.tsx
+++ src/components/PageHome/PageHome.tsx
@@ -6,11 +6,12 @@ import Project from "../Project/Project";
 export default function PageHome() {
   return (
     <>
-      <section className="">
+      <section className=" 
+      w-full ">
         <Navbar />
 
       </section>
-      <section className="max-w-[60rem] mx-auto mb-24 ">
+      <section className="max-w-[60rem] mx-auto mb-24 x">
 
         <div className="h-[100vh] flex items-center justify-center   w-full">
           <Title />
