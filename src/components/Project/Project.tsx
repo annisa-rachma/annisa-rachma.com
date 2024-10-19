bda81cb747dced2f366aaba3dbdeb4af9255ad3a
annisarachma <saa.annisa15@gmail.com>
2024-10-19 22:02:19 +0700

    fixing transition delay

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -10,24 +10,24 @@ export default function Project() {
 
       <div className="text-[1rem] md:text-[1.25rem] ">
 
-        <div className="border-y-[1px] border-white py-2 hover:bg-white hover:text-black cursor-pointer ">
-          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500 ">UI Playground</div>
+        <div className="border-y-[1px] border-white hover:bg-white hover:text-black cursor-pointer ">
+          <div className="py-2 hover:translate-x-4 transition-transform ease-in-out duration-500 ">UI Playground</div>
         </div>
 
-        <div className="border-b-[1px] border-white py-2 hover:bg-white hover:text-black cursor-pointer">
-          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500 ">Genshin Info</div>
+        <div className="border-b-[1px] border-white  hover:bg-white hover:text-black cursor-pointer">
+          <div className="py-2 hover:translate-x-4 transition-transform ease-in-out duration-500 ">Genshin Info</div>
         </div>
 
-        <div className="border-b-[1px] border-white py-2 hover:bg-white hover:text-black cursor-pointer">
-          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500 ">Kinfolk e-shop</div>
+        <div className="border-b-[1px] border-white hover:bg-white hover:text-black cursor-pointer">
+          <div className=" py-2 hover:translate-x-4 transition-transform ease-in-out duration-500 ">Kinfolk e-shop</div>
         </div>
 
-        <div className="border-b-[1px] border-white py-2 hover:bg-white hover:text-black cursor-pointer">
-          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500 ">Turu</div>
+        <div className="border-b-[1px] border-white  hover:bg-white hover:text-black cursor-pointer">
+          <div className="py-2 hover:translate-x-4 transition-transform ease-in-out duration-500 ">Turu</div>
         </div>
 
-        <div className="py-2 hover:bg-white hover:text-black cursor-pointer">
-          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500 ">Guess My Number</div>
+        <div className=" hover:bg-white hover:text-black cursor-pointer">
+          <div className="hover:translate-x-4 transition-transform ease-in-out duration-500  py-2">Guess My Number</div>
         </div>
 
       </div>
